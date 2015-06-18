2740fa0e38a767ec9ba804a2d7b7ea5a5595b0b0
ndegendogo <christa.runge@kifaru.de>
2015-06-18 22:58:35 +0200

    Improve portability: println instead of a newline character.
    This terminates the line with the system-specific line separator.
    Note: this was a feedback from Christian Hujer (Nelkinda).

--- Hello/Hello.java
+++ Hello/Hello.java
@@ -3,6 +3,6 @@ import java.lang.System;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.print("Hello, world!\n");
+        System.out.println("Hello, world!");
     }
 }
