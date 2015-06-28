6f3cdd29ea5ef680c3db0d8e2d4d054e4579338d
ndegendogo <christa.runge@kifaru.de>
2015-06-29 01:34:58 +0200

    Resolve it (green).

--- 006_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/ConvertToRoman.java
+++ 006_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/ConvertToRoman.java
@@ -19,8 +19,10 @@ public class ConvertToRoman {
     private void convert() {
         makeSequence(1000, "M");
         makeSequence(500, "D");
+        makeSequence(400, "CD");
         makeSequence(100, "C");
         makeSequence(50, "L");
+        makeSequence(40, "XL");
         makeSequence(10, "X");
         makeSequence(5, "V");
         makeSequence(4, "IV");
