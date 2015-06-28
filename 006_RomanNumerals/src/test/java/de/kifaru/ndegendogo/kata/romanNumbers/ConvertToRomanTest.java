88f176866fc29f4cf192dbb79e7ebad0bc729b1b
ndegendogo <christa.runge@kifaru.de>
2015-06-28 23:19:03 +0200

    Setup an Eclipse project with junit test.

---
+++ 006_RomanNumerals/src/test/java/de/kifaru/ndegendogo/kata/romanNumbers/ConvertToRomanTest.java
@@ -0,0 +1,22 @@
+package de.kifaru.ndegendogo.kata.romanNumbers;
+
+//import junit.framework.Test;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConvertToRomanTest {
+
+    @Test
+    public void testTrue() {
+      assertTrue( true );
+    }
+
+    @Test
+    public void testFalse() {
+      assertTrue( false );
+    }
+}
