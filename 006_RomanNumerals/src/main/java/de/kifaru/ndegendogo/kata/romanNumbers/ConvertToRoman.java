da90cb81a0cf52585c86eb0ac337212779640bde
ndegendogo <christa.runge@kifaru.de>
2015-06-29 00:07:34 +0200

    Implement sequence of II (green).

--- 006_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/ConvertToRoman.java
+++ 006_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/ConvertToRoman.java
@@ -2,8 +2,14 @@ package de.kifaru.ndegendogo.kata.romanNumbers;
 
 public class ConvertToRoman {
 
-    static String arabToRoman(String arabic) {
-        return "I";
+    static String arabToRoman(String arabicString) {
+        Integer arabicNumber = new Integer(arabicString);
+        StringBuilder roman = new StringBuilder();
+        while (arabicNumber > 0) {
+            roman.append("I");
+            arabicNumber -=1;
+        }
+        return roman.toString();
     }
 
     public static void main(String[] args) {
