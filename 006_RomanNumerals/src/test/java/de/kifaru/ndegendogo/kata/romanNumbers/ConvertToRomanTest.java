314a6162fc920a3ba7d1e06a597839035267a79d
ndegendogo <christa.runge@kifaru.de>
2015-07-11 08:38:32 +0200

    Refactor tests: prepare a class for test vectors.

--- 006_RomanNumerals/src/test/java/de/kifaru/ndegendogo/kata/romanNumbers/ConvertToRomanTest.java
+++ 006_RomanNumerals/src/test/java/de/kifaru/ndegendogo/kata/romanNumbers/ConvertToRomanTest.java
@@ -5,6 +5,20 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ConvertToRomanTest {
+    private class TestVector {
+        final int arabic;
+        final String expectedRoman;
+        TestVector(int arabic, String expectedRoman) {
+            this.arabic = arabic;
+            this.expectedRoman = expectedRoman;
+        }
+        int getArabic() {
+            return arabic;
+        }
+        String getExpectedRoman() {
+            return expectedRoman;
+        }
+    }
 
     @Test
     public void singleDigitI() {
