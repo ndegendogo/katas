0eeb84c83b39f69d822cca21475bbe93a48359ac
ndegendogo <christa.runge@kifaru.de>
2015-07-14 22:19:27 +0200

    Play with arrays: a few arrays declarations.

---
+++ 003_Testing/src/ArrayVariableTest.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+
+public class ArrayVariableTest {
+
+    // one-dimensional array declarations
+    int[] var1;
+    int var2[];     // this format is discouraged
+    Array var3;
+
+    // two-dimensional array declarations
+    int[][] var4;
+    int var5[][];   // this format is discouraged
+    int[] var6[];   // and this format is even more confusing
+}
