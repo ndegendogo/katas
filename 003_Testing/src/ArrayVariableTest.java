0c09ed613f1baf9ecbded28cb8758da02611ac21
ndegendogo <christa.runge@kifaru.de>
2015-07-14 22:53:13 +0200

    Prepare for unit tests (red).

--- 003_Testing/src/ArrayVariableTest.java
+++ 003_Testing/src/ArrayVariableTest.java
@@ -1,5 +1,9 @@
 import java.lang.reflect.Array;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class ArrayVariableTest {
 
     // one-dimensional array declarations
@@ -11,4 +15,10 @@ public class ArrayVariableTest {
     int[][] var4;
     int var5[][];   // this format is discouraged
     int[] var6[];   // and this format is even more confusing
+
+    @Test
+    public void test() {
+        fail();
+    }
+
 }
