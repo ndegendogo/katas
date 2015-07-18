95043d73dec37a90ff11a2815cf16f621cc05ab6
ndegendogo <christa.runge@kifaru.de>
2015-07-19 01:23:03 +0200

    Add definitions for arrays constructed at runtime.

--- 003_Testing/src/ArrayVariableTest.java
+++ 003_Testing/src/ArrayVariableTest.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -9,7 +10,7 @@ public class ArrayVariableTest {
     // one-dimensional array declarations
     int[] var1;
     int var2[];     // this format is discouraged
-    Array var3;
+//    Array var3;   => this is not an array!!
 
     // two-dimensional array declarations
     int[][] var4;
@@ -20,14 +21,22 @@ public class ArrayVariableTest {
     int[][] var7 = {{0, 1, 2}, {3, 4}, {5, 6, 7, 8}};
     Integer[][] var8 = {{0, 1, 2}, {3, 4}, {5, 6, 7, 8}};
     String[] var9 = {"the", "quick", "brown", "fox"};
+    int[] var10 = {1, 2, 3, 4, 7};
+    Integer[] var11 = {1, 2, 3, 4, 7};
+
+    // arrays allocated at runtime
+    int[] var12 = new int[4];
+    int[][] var13 = new int[5][6];
+    Integer[][] var14 = new Integer[3][];
+    
     
     @Test
     public void arrayDeclarationsDontAllocateObject() {
         assertNull(var1);
         assertNull(var2);
-        assertNull(var3);
         assertNull(var4);
         assertNull(var5);
         assertNull(var6);
     }
+    
 }
