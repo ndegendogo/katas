39ea83850cad1a1d217685c91f7d9dcbc50a9cf6
ndegendogo <christa.runge@kifaru.de>
2015-07-19 19:31:34 +0200

    Fix unit test (green).

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Echo.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Echo.java
@@ -7,8 +7,13 @@ public class Echo {
         System.out.println(resultString);
     }
 
-    public static String getResult(String param) {
-        final String resultString = param;
+    public static String getResult(String... params) {
+        String resultString;
+        if (params.length == 1) {
+            resultString = params[0];
+        } else {
+            resultString = params[0] + ' ' + params[1];
+        }
         return resultString;
     }
 
