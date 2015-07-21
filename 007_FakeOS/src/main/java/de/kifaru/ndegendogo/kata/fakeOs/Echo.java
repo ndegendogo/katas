4fa89935b3331c09e469973655f35df2513867ba
ndegendogo <christa.runge@kifaru.de>
2015-07-21 23:50:59 +0200

    Fix the red test (both, unit test and blackbox test).

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Echo.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Echo.java
@@ -12,7 +12,14 @@ public class Echo {
         for(String arg: args) {
             appendArg(builder, arg);
         }
-        return builder.toString().trim();
+        final String string = builder.toString();
+        String result;
+        if (args.length == 0) {
+            result = string;
+        } else {
+            result = string.substring(0, string.length() - 1);
+        }
+        return result;
     }
 
     public static void appendArg(StringBuilder builder, final String arg) {
