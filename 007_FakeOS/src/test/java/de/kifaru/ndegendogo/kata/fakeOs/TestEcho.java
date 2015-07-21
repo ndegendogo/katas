5abadfa14da0ff2b99cc6402d4e33d37e93379b4
ndegendogo <christa.runge@kifaru.de>
2015-07-22 00:14:54 +0200

    Cleanup tests - swap arguments in the assertion.

--- 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestEcho.java
+++ 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestEcho.java
@@ -9,36 +9,36 @@ public class TestEcho {
     @Test
     public void singleArgument() {
         final String result = Echo.getResult("hello");
-        assertEquals(result, "hello");
+        assertEquals("hello", result);
     }
 
     @Test
     public void anotherSingleArgument() {
         final String result = Echo.getResult("world");
-        assertEquals(result, "world");
+        assertEquals("world", result);
     }
     
     @Test
     public void twoArguments() {
         final String result = Echo.getResult("Hello", "world");
-        assertEquals(result, "Hello world");
+        assertEquals("Hello world", result);
     }
 
     @Test
     public void noArgument() {
         final String result = Echo.getResult();
-        assertEquals(result, "");
+        assertEquals("", result);
     }
 
     @Test
     public void argumentWithTrailingSpace() {
         final String result = Echo.getResult("hello ");
-        assertEquals(result, "hello ");
+        assertEquals("hello ", result);
     }
 
     @Test
     public void emptyArgument() {
         final String result = Echo.getResult("");
-        assertEquals(result, "");
+        assertEquals("", result);
     }
 }
