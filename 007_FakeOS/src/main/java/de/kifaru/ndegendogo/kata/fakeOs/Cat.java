a091ef15f8cf10212b134c81b798eb7e4ce1a9c2
ndegendogo <christa.runge@kifaru.de>
2015-07-26 11:21:42 +0200

    Extend production code to fulfill the first blackbox test case (green).
    Note: extremely simple hardcoded implementation.

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Cat.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Cat.java
@@ -3,8 +3,7 @@ package de.kifaru.ndegendogo.kata.fakeOs;
 public class Cat {
 
     public static void main(String... args) {
-        // TODO Auto-generated method stub
-
+        System.out.print("This is one line of text.\n");
     }
 
 }
