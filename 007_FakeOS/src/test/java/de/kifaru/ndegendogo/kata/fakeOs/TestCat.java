90a1154770a1666b20d077f66a8ac997ed4f5919
ndegendogo <christa.runge@kifaru.de>
2015-07-26 17:33:18 +0200

    Add a unit test file to test Cat (green).

---
+++ 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestCat.java
@@ -0,0 +1,14 @@
+package de.kifaru.ndegendogo.kata.fakeOs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCat {
+
+    @Test
+    public void singleFileWithOneLine() {
+        assertEquals("This is one line of text.\n", Cat.getContent());
+    }
+    
+}
