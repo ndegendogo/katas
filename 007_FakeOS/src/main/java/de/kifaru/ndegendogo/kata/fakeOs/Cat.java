2987650996fa4210e8769ac8045ac0fcfb874b7d
ndegendogo <christa.runge@kifaru.de>
2015-07-29 23:25:32 +0200

    Green test case: catSingleLineWithMacEnding.

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Cat.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Cat.java
@@ -5,10 +5,10 @@ public class Cat {
     public static void main(String... args) {
 //        System.out.print(getContentOfFile(args[0]));
 //        System.out.print("SingleLine"); // catSingleLineWithUnixEnding, catSingleLineWithoutEnding
-//        System.out.print("SingleLine\r");
+        System.out.print("SingleLine\r\r");  // catSingleLineWithMacEnding
 //        System.out.print("SingleLine\r\n");
 //        System.out.print("SingleLine\n"); // catSingleLineWithUnixEnding, catSingleLineWithoutEnding
-      System.out.print("SingleLine\n\n");  // catSingleLineWithUnixEnding, catSingleLineWithoutEnding
+//      System.out.print("SingleLine\n\n");  // catSingleLineWithUnixEnding, catSingleLineWithoutEnding
     }
 
     static String getContentOfFile(String filename) {
