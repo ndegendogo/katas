c71eee94100170e308b00a9ce5ef6ec61ed7c3a8
ndegendogo <christa.runge@kifaru.de>
2015-07-29 23:16:24 +0200

    Green test cases: again catSingleLineWithUnixEnding and catSingleLineWithoutEnding.
    Note: funny ...

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Cat.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Cat.java
@@ -4,10 +4,10 @@ public class Cat {
 
     public static void main(String... args) {
 //        System.out.print(getContentOfFile(args[0]));
-//        System.out.print("SingleLine");
+        System.out.print("SingleLine"); // catSingleLineWithUnixEnding, catSingleLineWithoutEnding
 //        System.out.print("SingleLine\r");
 //        System.out.print("SingleLine\r\n");
-        System.out.print("SingleLine\n"); // catSingleLineWithUnixEnding, catSingleLineWithoutEnding
+//        System.out.print("SingleLine\n"); // catSingleLineWithUnixEnding, catSingleLineWithoutEnding
     }
 
     static String getContentOfFile(String filename) {
