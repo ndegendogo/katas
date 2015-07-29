22994a3bcb650bcac282fe6ee1b145b66bc09d1a
ndegendogo <christa.runge@kifaru.de>
2015-07-29 23:28:24 +0200

    Green test case: catSingleLineWithWindowsEnding.

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Cat.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Cat.java
@@ -5,8 +5,8 @@ public class Cat {
     public static void main(String... args) {
 //        System.out.print(getContentOfFile(args[0]));
 //        System.out.print("SingleLine"); // catSingleLineWithUnixEnding, catSingleLineWithoutEnding
-        System.out.print("SingleLine\r\r");  // catSingleLineWithMacEnding
-//        System.out.print("SingleLine\r\n");
+//        System.out.print("SingleLine\r\r");  // catSingleLineWithMacEnding
+        System.out.print("SingleLine\r\n\r");  // catSingleLineWithWindowsEnding
 //        System.out.print("SingleLine\n"); // catSingleLineWithUnixEnding, catSingleLineWithoutEnding
 //      System.out.print("SingleLine\n\n");  // catSingleLineWithUnixEnding, catSingleLineWithoutEnding
     }
