aecf62d89d96bc355007c66991b49b45123fcbd1
ndegendogo <christa.runge@kifaru.de>
2015-08-01 21:01:22 +0200

    Refactor unit test: extract a method.

--- 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestCat.java
+++ 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestCat.java
@@ -10,13 +10,17 @@ public class TestCat {
     public void singleFileWithOneLine() {
         final String expectedContent = "This is one line of text.\n";
         final String filename = "data/file1";
-        assertEquals(expectedContent, Cat.getContentOfFile(filename));
+        testContentOfFile(expectedContent, filename);
     }
-    
+
     @Test
     public void anotherSingleFileWithSingleLine() {
         final String expectedContent = "This is another line of text.\n";
         final String filename = "data/file2";
+        testContentOfFile(expectedContent, filename);
+    }
+
+    private void testContentOfFile(final String expectedContent, final String filename) {
         assertEquals(expectedContent, Cat.getContentOfFile(filename));
     }
 }
