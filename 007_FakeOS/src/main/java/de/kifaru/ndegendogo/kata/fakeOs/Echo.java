adad9021db34f900527927c1cb2c76dceac9cc09
ndegendogo <christa.runge@kifaru.de>
2015-08-16 00:56:23 +0200

    Fix the production code (green).

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Echo.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Echo.java
@@ -3,9 +3,7 @@ package de.kifaru.ndegendogo.kata.fakeOs;
 public class Echo {
 
     public static void main(final String... args) {
-        if(args[0].length() > 0) {
-            System.out.println(joinStrings(args));
-        }
+        System.out.println(joinStrings(args));
     }
 
     static String joinStrings(final String... strings) {
