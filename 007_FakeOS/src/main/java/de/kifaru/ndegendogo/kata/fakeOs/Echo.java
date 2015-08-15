c7a94a1e0c773ce34aa54780f648d54f4566eb8e
ndegendogo <christa.runge@kifaru.de>
2015-08-16 00:14:02 +0200

    Introduce a bug in Echo - blackbox test is still green.
    Note: Actually, I wanted to break the blackbox test 'echoWithoutParametersPrintsNothing'.
    The previous refactoring of the makefiles had introduced an additional space into this test,
    and I wanted to verify that it still works.
    Result: Echo throws an exception as expected, but the makefile continues.
    Need to improve the test.

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Echo.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Echo.java
@@ -3,7 +3,9 @@ package de.kifaru.ndegendogo.kata.fakeOs;
 public class Echo {
 
     public static void main(final String... args) {
-        System.out.println(joinStrings(args));
+        if(args[0].length() > 0) {
+            System.out.println(joinStrings(args));
+        }
     }
 
     static String joinStrings(final String... strings) {
