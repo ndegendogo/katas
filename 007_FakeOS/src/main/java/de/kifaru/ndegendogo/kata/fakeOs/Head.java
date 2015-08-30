2812e8d45608c48c48e0b8f09dce6a1eb9c02c2e
ndegendogo <christa.runge@kifaru.de>
2015-08-30 23:34:35 +0200

    resolve it (green).

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Head.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Head.java
@@ -1,8 +1,23 @@
 package de.kifaru.ndegendogo.kata.fakeOs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.System;
+
 public class Head {
 
-    public static void main(final String... args) {
+    public static void main(final String... args) throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader buffered = new BufferedReader(reader);
+        try {
+            String line = buffered.readLine();
+            if (line != null) {
+                System.out.println(line);
+            }
+        } catch (IOException exception) {
+            
+        }
     }
 
 }
