71457341a075b3d2fa459a7e8856f718519bb9b1
ndegendogo <christa.runge@kifaru.de>
2015-08-30 23:43:44 +0200

    Refactor production code: extract two local variables.

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Head.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Head.java
@@ -2,17 +2,21 @@ package de.kifaru.ndegendogo.kata.fakeOs;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.lang.System;
 
 public class Head {
 
     public static void main(final String... args) throws IOException {
-        InputStreamReader reader = new InputStreamReader(System.in);
+        final InputStream in = System.in;
+        final PrintStream out = System.out;
+        InputStreamReader reader = new InputStreamReader(in);
         BufferedReader buffered = new BufferedReader(reader);
         String line = buffered.readLine();
         if (line != null) {
-            System.out.println(line);
+            out.println(line);
         }
     }
 
