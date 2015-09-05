0b9273102357a3d4f89c9db19743e31ee4902a9b
ndegendogo <christa.runge@kifaru.de>
2015-09-05 17:34:46 +0200

    Verify refactoring of blackbox tests (sabotage).
    catOneFileWithSingleLine detects wrong exit status as expected (red).

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Cat.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Cat.java
@@ -20,6 +20,7 @@ public class Cat {
         if (!result) {
             System.exit(1);
         }
+        System.exit(1);
     }
 
     private static String[] getFilenames(final String... args) {
