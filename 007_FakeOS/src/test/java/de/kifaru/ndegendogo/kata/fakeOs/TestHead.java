3943ea8e0683847b56e94ffd3ec940e3581f182d
ndegendogo <christa.runge@kifaru.de>
2015-09-06 00:51:30 +0200

    setup unit tests for Head.java (red).

---
+++ 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestHead.java
@@ -0,0 +1,13 @@
+package de.kifaru.ndegendogo.kata.fakeOs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestHead {
+
+    @Test
+    public void testPrintLines() {
+        fail();
+    }
+}
