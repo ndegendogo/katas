ca820bc463bd0b2f72e55ea3babb835f1364b5ea
ndegendogo <christa.runge@kifaru.de>
2015-09-06 00:59:55 +0200

    Improve unit test: call the production code (still red).
    It throws a NullPointerException as expected.

--- 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestHead.java
+++ 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestHead.java
@@ -2,12 +2,23 @@ package de.kifaru.ndegendogo.kata.fakeOs;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 import org.junit.Test;
 
 public class TestHead {
 
     @Test
     public void testPrintLines() {
+        final InputStream in = null;
+        final PrintStream out = null;
+        try {
+            Head.printLines(in, out);
+        } catch (IOException e) {
+            fail();
+        }
         fail();
     }
 }
