ab96f9368070e866aae0da9623ca5dd6e75ab3d0
ndegendogo <christa.runge@kifaru.de>
2015-09-06 01:34:14 +0200

    Refactor unit test, make it Parametrized.

--- 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestHead.java
+++ 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestHead.java
@@ -6,13 +6,32 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Arrays;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(value = Parameterized.class)
 public class TestHead {
 
-    private final String inputString = "";
-    private final String expected = "";
+    @Parameters(name = "{0}")
+    public static Iterable<Object[]>data() {
+        return Arrays.asList(new Object[][] {
+            {"noLines", "", ""}
+        });
+    }
+    
+    private final String testName;
+    private final String inputString;
+    private final String expected;
+    
+    public TestHead(final String testName, final String inputString, final String expected) {
+        this.testName = testName;
+        this.inputString = inputString;
+        this.expected = expected;
+    }
     
     @Test
     public void testPrintLines() throws IOException {
