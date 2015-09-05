cd0d01374dedbe6edcf80a11acacc080bcfb72c3
ndegendogo <christa.runge@kifaru.de>
2015-09-06 01:17:31 +0200

    Finish first unit test for Head, it is green now.

--- 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestHead.java
+++ 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestHead.java
@@ -2,23 +2,24 @@ package de.kifaru.ndegendogo.kata.fakeOs;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintStream;
 
 import org.junit.Test;
 
 public class TestHead {
 
+    private final String inputString = "";
+    private final String expected = "";
+    
     @Test
-    public void testPrintLines() {
-        final InputStream in = null;
-        final PrintStream out = null;
-        try {
-            Head.printLines(in, out);
-        } catch (IOException e) {
-            fail();
-        }
-        fail();
+    public void testPrintLines() throws IOException {
+        final ByteArrayInputStream input = new ByteArrayInputStream(inputString.getBytes());
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        final PrintStream out = new PrintStream(output);
+        Head.printLines(input, out);
+        assertEquals(expected, output.toString());
     }
 }
