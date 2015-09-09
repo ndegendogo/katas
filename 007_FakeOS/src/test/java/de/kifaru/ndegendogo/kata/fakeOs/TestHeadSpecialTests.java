73d3bcec04fc1ca9a03690a5321c77d84dce0a05
ndegendogo <christa.runge@kifaru.de>
2015-09-10 00:30:54 +0200

    Refactor unit tests: make BaseTestHead base class of the other TestHead.... classes.

--- 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestHeadSpecialTests.java
+++ 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestHeadSpecialTests.java
@@ -14,7 +14,7 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(value = Parameterized.class)
-public class TestHeadSpecialTests {
+public class TestHeadSpecialTests extends BaseTestHead {
 
     @Parameters(name = "{0}")
     public static Iterable<Object[]>data() {
