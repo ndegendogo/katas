596a2cda811823c4d77d0a3f32ea3381153a3761
ndegendogo <christa.runge@kifaru.de>
2015-09-10 01:26:47 +0200

    Refactor unit tests: move the testcase moreLinesThanHeadOutputs to TestHeadParameterized.

--- 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestHeadSpecialTests.java
+++ 007_FakeOS/src/test/java/de/kifaru/ndegendogo/kata/fakeOs/TestHeadSpecialTests.java
@@ -19,33 +19,6 @@ public class TestHeadSpecialTests extends BaseTestHead {
     @Parameters(name = "{0}")
     public static Iterable<Object[]>data() {
         return Arrays.asList(new Object[][] {
-                {"moreLinesThanHeadOutputs", 
-                    new String[]{
-                        "line 1",
-                        "line 2",
-                        "line 3",
-                        "line 4",
-                        "line 5",
-                        "line 6",
-                        "line 7",
-                        "line 8",
-                        "line 9",
-                        "line 10",
-                        "line 11",
-                    }, 
-                    new String[]{
-                        "line 1",
-                        "line 2",
-                        "line 3",
-                        "line 4",
-                        "line 5",
-                        "line 6",
-                        "line 7",
-                        "line 8",
-                        "line 9",
-                        "line 10",
-                    }
-                },
         });
     }
     
