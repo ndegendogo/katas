a06016348017f7f612e50b55f1e5053427b49b01
ndegendogo <christa.runge@kifaru.de>
2015-09-09 14:06:26 +0200

    Refactor production code: make the loop more compact.

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Head.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Head.java
@@ -27,13 +27,9 @@ public class Head {
     static void print2Lines(final InputStream in, final PrintStream out) throws IOException {
         final InputStreamReader reader = new InputStreamReader(in);
         final BufferedReader buffered = new BufferedReader(reader);
-        do {
-            final String line = buffered.readLine();
-            if (line != null) {
-                out.println(line);
-            } else {
-                break;
-            }
-        } while (true);
+        String line;
+        while((line = buffered.readLine()) != null) {
+            out.println(line);
+        }
     }
 }
