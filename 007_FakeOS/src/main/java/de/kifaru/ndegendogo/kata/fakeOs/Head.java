19a4d899ae3d634db379abbec985f2878cfa750c
ndegendogo <christa.runge@kifaru.de>
2015-09-10 17:08:07 +0200

    Play with the production code - try to refactor it into a Collection pipeline, using Streams (ongoing).
    See http://martinfowler.com/articles/refactoring-pipelines.html.

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Head.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Head.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.lang.System;
+import java.util.stream.Stream;
 
 public class Head {
 
@@ -20,9 +21,11 @@ public class Head {
     static void printLines(final InputStream in, final PrintStream out) throws IOException {
         final InputStreamReader reader = new InputStreamReader(in);
         final BufferedReader buffered = new BufferedReader(reader);
-        String line;
+        final Stream<String> lines = buffered.lines();
+        Object[] array = lines.toArray();
+
         int lineCount = 0;
-        while((line = buffered.readLine()) != null) {
+        for (Object line : array) {
             if (++lineCount <= MAX_NUMBER_OF_LINES) {
                 out.println(line);
             }
