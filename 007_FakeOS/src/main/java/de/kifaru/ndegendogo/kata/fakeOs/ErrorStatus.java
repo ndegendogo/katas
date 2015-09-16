bdb9cbe95c5dc2eb8ebc3650c576397c80fa094a
ndegendogo <christa.runge@kifaru.de>
2015-09-17 00:30:23 +0200

    Add a class ErrorStatus.

---
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/ErrorStatus.java
@@ -0,0 +1,8 @@
+package de.kifaru.ndegendogo.kata.fakeOs;
+
+public class ErrorStatus {
+    private boolean hasError = false;
+    
+    public void setError() {hasError = true;}
+    public boolean isError() {return hasError;}
+}
