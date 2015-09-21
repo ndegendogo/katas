e851f2c33e25f15b49f72db59c3da177739aad35
ndegendogo <christa.runge@kifaru.de>
2015-09-21 21:30:04 +0200

    Refactor Cat: cleanup exception handling.
    Catch the expected exceptions as early as possible.

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Cat.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Cat.java
@@ -2,7 +2,6 @@ package de.kifaru.ndegendogo.kata.fakeOs;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -21,7 +20,7 @@ public class Cat {
         this.output = output;
     }
 
-    public static void main(final String... args) throws FileNotFoundException, IOException {
+    public static void main(final String... args) {
         final Cat cat = new Cat(System.in, System.out);
         if (args.length == 0) {
             cat.processDefault();
@@ -39,35 +38,37 @@ public class Cat {
     }
 
     private void processSingle(final String name) {
-        try {
-            if(DEFAULT_INPUT.equals(name)) {
-                processDefault();
-            } else {
-                processSingleFile(name);
-            }
+        if(DEFAULT_INPUT.equals(name)) {
+            processDefault();
+        } else {
+            processSingleFile(name);
+        }
+    }
+
+    void processDefault() {
+        writeStreamToOutput(defaultInput);
+    }
+
+    void processSingleFile(final String filename)  {
+        try (FileInputStream input = new FileInputStream(filename);
+            BufferedInputStream bufferedIn = new BufferedInputStream(input); 
+        ) {
+            writeStreamToOutput(bufferedIn);
         } catch (IOException e) {
             hasError = true;
         }
     }
 
-    void processDefault() throws IOException {
-        writeStreamToOutput(defaultInput);
-    }
-
-    void processSingleFile(final String filename) throws IOException {
-        try (FileInputStream input = new FileInputStream(filename);
-            BufferedInputStream bufferedIn = new BufferedInputStream(input); 
-        ) {
-            writeStreamToOutput(bufferedIn);
+    private void writeStreamToOutput(final InputStream source) {
+        try {
+            int nextByte;
+            while((nextByte = source.read()) != -1) {
+                output.write(nextByte);
+            }
+            output.flush();
+        } catch (IOException e) {
+            hasError = true;
         }
-    }
-
-    private void writeStreamToOutput(final InputStream source) throws IOException {
-        int nextByte;
-        while((nextByte = source.read()) != -1) {
-            output.write(nextByte);
-        }
-        output.flush();
     }
 
     boolean hasError() {
