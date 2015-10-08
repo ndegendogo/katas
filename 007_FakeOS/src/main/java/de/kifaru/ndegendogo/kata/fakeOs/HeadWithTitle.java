723f1cea943ded98c30284ebe92a4e29ba1673cd
ndegendogo <christa.runge@kifaru.de>
2015-10-08 23:50:54 +0200

    HeadWithTitle: add field for the current filename.
    This allows to unify the signature of the process method.

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/HeadWithTitle.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/HeadWithTitle.java
@@ -7,25 +7,28 @@ import java.io.InputStream;
 import java.io.PrintStream;
 
 public class HeadWithTitle extends Head {
+    
+    String currentFilename;
 
     HeadWithTitle (final InputStream in, final PrintStream out) {
         super(in, new OutputJoiner(out));
     }
 
     protected void processSingleFile(final String filename) {
+        currentFilename = filename;
         try (
             final FileReader fileReader = new FileReader(filename);
             final BufferedReader bufferedReader = new BufferedReader(fileReader);
         ) {
-            process(bufferedReader, filename);
+            process(bufferedReader);
         } catch (IOException e) {
             setError();
         }
     }
 
-    private void process(final BufferedReader bufferedReader, final String filename) {
+    protected void process(final BufferedReader bufferedReader) {
         final String leadingLines = readLeadingLines(bufferedReader);
-        output.print(buildTitle(filename));
+        output.print(buildTitle(currentFilename));
         output.print(leadingLines);
     }
 
