60af8efc02c41398a4405f542dd29782d76bfb46
ndegendogo <christa.runge@kifaru.de>
2015-10-11 22:39:15 +0200

    Add another implementation of FileOperation for a text file.

---
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/TextFileOperation.java
@@ -0,0 +1,32 @@
+package de.kifaru.ndegendogo.kata.fakeOs;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.function.Consumer;
+
+public class TextFileOperation implements FileOperation {
+
+    private final BufferedReader defaultInput;
+    private final  Consumer<BufferedReader> process;
+
+    public TextFileOperation(BufferedReader defaultInput, Consumer<BufferedReader> process) {
+        this.defaultInput = defaultInput;
+        this.process = process;
+    }
+
+    @Override
+    public void processFromDefault() {
+        process.accept(defaultInput);
+    }
+
+    @Override
+    public void processFromFile(String filename) throws IOException {
+        try (
+            final FileReader fileReader = new FileReader(filename);
+            final BufferedReader bufferedReader = new BufferedReader(fileReader);
+        ) {
+            process.accept(bufferedReader);
+        }
+    }
+}
