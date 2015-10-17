5a249a44d39944bfba5af4bc8947a5a675e63ec4
ndegendogo <christa.runge@kifaru.de>
2015-10-17 13:44:09 +0200

    Define new class FileCommandProcessor to be used with the dependency inversion.

---
+++ 007_FakeOS_variant2/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/FileCommandProcessor.java
@@ -0,0 +1,10 @@
+package de.kifaru.ndegendogo.kata.fakeOs;
+
+public class FileCommandProcessor {
+
+    final FileCommand command;
+
+    FileCommandProcessor(FileCommand command) {
+        this.command = command;
+    }
+}
