84d68ff851fb830246e750724851230e8306b8f6
ndegendogo <christa.runge@kifaru.de>
2015-11-14 19:29:31 +0100

    Implement it (testcase is green now).

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Tail.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Tail.java
@@ -1,8 +1,24 @@
 package de.kifaru.ndegendogo.kata.fakeOs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+
 public class Tail {
 
-    public static void main(String... args) {
+    public static void main(String... args) throws IOException {
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+        BufferedReader buffered = new BufferedReader(reader);
+        String line = buffered.readLine();
+        if (line != null) {
+            PrintStream out = System.out;
+            out.println(line);
+            out.flush();
+        }
     }
 
 }
