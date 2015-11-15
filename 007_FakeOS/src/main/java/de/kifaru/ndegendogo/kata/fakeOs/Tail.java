1c146d09a254c500afef553765b75ab1e9bbdc11
ndegendogo <christa.runge@kifaru.de>
2015-11-15 17:58:53 +0100

    Refactor: unify Tail with Head as much as possible.

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Tail.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Tail.java
@@ -8,19 +8,18 @@ import java.io.PrintStream;
 import java.util.concurrent.ArrayBlockingQueue;
 
 
-public class Tail {
+public class Tail extends FileCommand {
     private static final int MAX_NUMBER_OF_LINES = 10;
     private final PrintStream output;
 
-    Tail(final PrintStream output) {
+    Tail(final InputStream in, final PrintStream output) {
         this.output = output;
+        setFileOperation(new TextFileOperation(in, this::printTrailingLines));
     }
     
     public static void main(final String... args) throws IOException {
-        final InputStream in = System.in;
-        final PrintStream out = System.out;
-        
-        new Tail(out).process(in);
+        final FileCommand tail = new Tail(System.in, System.out);
+        tail.processAll(args);
     }
 
     void process(final InputStream in) throws IOException {
@@ -31,14 +30,18 @@ public class Tail {
         }
     }
 
-    void printTrailingLines(final BufferedReader buffered) throws IOException {
+    void printTrailingLines(final BufferedReader buffered) {
         final ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(MAX_NUMBER_OF_LINES);
         String line;
-        while ((line = buffered.readLine()) != null) {
-            if (queue.remainingCapacity() == 0) {
-                queue.remove();
+        try {
+            while ((line = buffered.readLine()) != null) {
+                if (queue.remainingCapacity() == 0) {
+                    queue.remove();
+                }
+                queue.add(new String(line));
             }
-            queue.add(new String(line));
+        } catch (IOException e) {
+            setError();
         }
         queue.forEach(output::println);
     }
