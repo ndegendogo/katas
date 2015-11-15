cdaf7d1c0369b89dce2651a82eb58cf38bd3a14a
ndegendogo <christa.runge@kifaru.de>
2015-11-15 17:14:33 +0100

    Tail: refactor.
    Use a queue with the desired capacity.

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Tail.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/Tail.java
@@ -1,17 +1,33 @@
 package de.kifaru.ndegendogo.kata.fakeOs;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.concurrent.ArrayBlockingQueue;
 
 
 public class Tail {
+    private static final int MAX_NUMBER_OF_LINES = 10;
 
     public static void main(final String... args) throws IOException {
         final InputStream in = System.in;
         final PrintStream out = System.out;
         
-        new Head(in, out).processAll();
+        new Tail().process(in, out);
     }
 
+    void process(final InputStream in, final PrintStream out) throws IOException {
+        try(final InputStreamReader reader = new InputStreamReader(in);
+            final BufferedReader buffered = new BufferedReader(reader);
+        ) {
+            final ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(MAX_NUMBER_OF_LINES);
+            String line;
+            while ((line = buffered.readLine()) != null) {
+                queue.add(new String(line));
+            }
+            queue.forEach(out::println);
+        }
+    }
 }
