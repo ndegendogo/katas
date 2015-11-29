23ea17e896a1862a9fa62c8a43d594d4e99ef9d5
ndegendogo <christa.runge@kifaru.de>
2015-11-29 16:33:18 +0100

    Refactor: move buildTitle to BaseHeadTail.

--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/BaseHeadTail.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/BaseHeadTail.java
@@ -2,4 +2,8 @@ package de.kifaru.ndegendogo.kata.fakeOs;
 
 public class BaseHeadTail extends FileCommand {
 
+    protected String buildTitle(final String filename) {
+        return "==> " + filename + " <==";
+    }
+
 }
--- 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/FileCommand.java
+++ 007_FakeOS/src/main/java/de/kifaru/ndegendogo/kata/fakeOs/FileCommand.java
@@ -44,10 +44,6 @@ public class FileCommand {
         }
     }
 
-    protected String buildTitle(final String filename) {
-        return "==> " + filename + " <==";
-    }
-
     protected boolean setError() {
         return hasError = true;
     }
