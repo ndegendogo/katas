b71b45b1ebe5a6da0dc326b2dafeef4847824475
ndegendogo <christa.runge@kifaru.de>
2016-05-06 15:21:12 +0200

    Create a new project.

---
+++ 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/ConvertNumber.java
@@ -0,0 +1,9 @@
+package de.kifaru.ndegendogo.kata.romanNumbers;
+
+public class ConvertNumber {
+
+    public static void main(String ... args) {
+        System.out.println("Hello World!");
+    }
+
+}
