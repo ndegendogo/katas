1ff6671c3b0d12bff0b9c457ef68e1009401ba34
ndegendogo <christa.runge@kifaru.de>
2016-05-07 15:39:41 +0200

    Add a layout.

--- 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/ConvertNumber.java
+++ 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/ConvertNumber.java
@@ -1,6 +1,11 @@
 package de.kifaru.ndegendogo.kata.romanNumbers;
 
+
+import java.awt.Container;
+
 import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.GroupLayout.SequentialGroup;
 
 public class ConvertNumber {
 
@@ -13,12 +18,24 @@ public class ConvertNumber {
     }
 
     private static void createAndShowGui() {
+        // top-level container
         JFrame frame = new JFrame("Roman Numbers");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         JLabel label = new JLabel("Hello World!");
-        frame.getContentPane().add(label);
-        
+
+        Container pane = frame.getContentPane();
+        GroupLayout layout = new GroupLayout(pane);
+        pane.setLayout(layout);
+
+        SequentialGroup hgroup = layout.createSequentialGroup();
+        hgroup.addGroup(layout.createParallelGroup().addComponent(label));
+        layout.setHorizontalGroup(hgroup);
+
+        SequentialGroup vgroup = layout.createSequentialGroup();
+        vgroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).addComponent(label));
+        layout.setVerticalGroup(vgroup);
+
         frame.pack();
         frame.setVisible(true);
     }
