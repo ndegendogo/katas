f61fcd59e7f7d62ccb89fb66fd2ab85009656433
ndegendogo <christa.runge@kifaru.de>
2016-05-15 13:47:30 +0200

    Add a class for unit test (red).

---
+++ 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverterTest.java
@@ -0,0 +1,14 @@
+package de.kifaru.ndegendogo.kata.romanNumbers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RomanNumbersConverterTest {
+
+    @Test
+    public void testConvertToArabicNumber() {
+        fail("Not yet implemented");
+    }
+
+}
