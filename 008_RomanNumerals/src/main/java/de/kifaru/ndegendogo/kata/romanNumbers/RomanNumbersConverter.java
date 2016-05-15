6605ca505099cf8d8209fa8dc5e92f8db0e8ca76
ndegendogo <christa.runge@kifaru.de>
2016-05-15 11:26:59 +0200

    Refactor: add a class RomanNumbersConverter.

---
+++ 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverter.java
@@ -0,0 +1,13 @@
+package de.kifaru.ndegendogo.kata.romanNumbers;
+
+import java.util.function.Function;
+
+public class RomanNumbersConverter implements Function<String, Integer> {
+
+    @Override
+    public Integer apply(String t) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
