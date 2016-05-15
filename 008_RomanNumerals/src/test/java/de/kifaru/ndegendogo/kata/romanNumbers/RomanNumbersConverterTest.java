dd926159e7df8511a442f1bd483fa66827972a89
ndegendogo <christa.runge@kifaru.de>
2016-05-15 22:28:10 +0200

    Refactor unit test: extract a local variable.

--- 008_RomanNumerals/src/test/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverterTest.java
+++ 008_RomanNumerals/src/test/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverterTest.java
@@ -8,8 +8,9 @@ public class RomanNumbersConverterTest {
 
     @Test
     public void testConvertToArabicNumber() {
+        final String actualInput = "I";
         final int expectedResult = 1;
-        final Integer actualResult = RomanNumbersConverter.convertToArabicNumber("I");
+        final Integer actualResult = RomanNumbersConverter.convertToArabicNumber(actualInput);
         assertEquals(expectedResult, actualResult.intValue());
     }
 
