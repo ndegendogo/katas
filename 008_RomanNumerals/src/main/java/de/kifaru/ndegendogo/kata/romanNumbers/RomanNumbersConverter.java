d5d62d09a5c1a73e7387158a7e0a6235077e5559
ndegendogo <christa.runge@kifaru.de>
2016-05-16 18:04:23 +0200

    Extend production code: make test case green.

--- 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverter.java
+++ 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverter.java
@@ -14,6 +14,9 @@ public class RomanNumbersConverter implements Function<String, Integer> {
         int result = 0;
         offset = 0;
         if (romanNumber.length() > offset) {
+            result += matchDigit10(romanNumber);
+        }
+        if (romanNumber.length() > offset) {
             result += matchDigit5(romanNumber);
         }
         while (romanNumber.length() > offset) {
@@ -36,6 +39,16 @@ public class RomanNumbersConverter implements Function<String, Integer> {
         int value;
         if (romanNumber.charAt(offset) == 'V') {
             value = 5;
+            offset += 1;
+        } else {
+            value = 0;
+        }
+        return value;
+    }
+    private int matchDigit10(final String romanNumber) {
+        int value;
+        if (romanNumber.charAt(offset) == 'X') {
+            value = 10;
             offset += 1;
         } else {
             value = 0;
