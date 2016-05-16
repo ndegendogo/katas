6419375e1ff34e316bbb56e3a295a26293e5b16b
ndegendogo <christa.runge@kifaru.de>
2016-05-17 00:32:36 +0200

    Extend production code, implement new test case (green).

--- 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverter.java
+++ 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverter.java
@@ -9,7 +9,10 @@ public class RomanNumbersConverter implements Function<String, Integer> {
     private String romanNumber;
 
     public enum RomanDigit {
-        DIGIT_L('L', 50), DIGIT_X('X', 10), DIGIT_V('V', 5), DIGIT_I('I', 1);
+        DIGIT_C('C', 100), 
+        DIGIT_L('L', 50), DIGIT_X('X', 10),
+        DIGIT_V('V', 5), DIGIT_I('I', 1);
+
         private final char digit;
         private final int value;
         private RomanDigit(char digit, int value) {
