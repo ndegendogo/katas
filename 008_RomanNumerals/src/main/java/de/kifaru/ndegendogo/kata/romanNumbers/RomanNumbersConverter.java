3d805403f932fd1aba4b3e92d1d4379330ea32ba
ndegendogo <christa.runge@kifaru.de>
2016-05-16 18:10:24 +0200

    Refactor production code: unify methods, add a parameter.

--- 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverter.java
+++ 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverter.java
@@ -14,20 +14,20 @@ public class RomanNumbersConverter implements Function<String, Integer> {
         int result = 0;
         offset = 0;
         if (romanNumber.length() > offset) {
-            result += matchDigit10(romanNumber);
+            result += matchDigit10(romanNumber, 'X');
         }
         if (romanNumber.length() > offset) {
-            result += matchDigit5(romanNumber);
+            result += matchDigit5(romanNumber, 'V');
         }
         while (romanNumber.length() > offset) {
-            result += matchDigit(romanNumber);
+            result += matchDigit(romanNumber, 'I');
         }
         return result;
     }
 
-    private int matchDigit(final String romanNumber) {
+    private int matchDigit(final String romanNumber, char digit) {
         int value;
-        if (romanNumber.charAt(offset) == 'I') {
+        if (romanNumber.charAt(offset) == digit) {
             value = 1;
             offset += 1;
         } else {
@@ -35,9 +35,9 @@ public class RomanNumbersConverter implements Function<String, Integer> {
         }
         return value;
     }
-    private int matchDigit5(final String romanNumber) {
+    private int matchDigit5(final String romanNumber, char digit) {
         int value;
-        if (romanNumber.charAt(offset) == 'V') {
+        if (romanNumber.charAt(offset) == digit) {
             value = 5;
             offset += 1;
         } else {
@@ -45,9 +45,9 @@ public class RomanNumbersConverter implements Function<String, Integer> {
         }
         return value;
     }
-    private int matchDigit10(final String romanNumber) {
+    private int matchDigit10(final String romanNumber, char digit) {
         int value;
-        if (romanNumber.charAt(offset) == 'X') {
+        if (romanNumber.charAt(offset) == digit) {
             value = 10;
             offset += 1;
         } else {
