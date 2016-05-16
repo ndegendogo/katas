482b2c5772b05eb273d98feb9234e339f91ca518
ndegendogo <christa.runge@kifaru.de>
2016-05-16 16:24:32 +0200

    Refactor the unit test: prepare a parametrized test.

--- 008_RomanNumerals/src/test/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverterTest.java
+++ 008_RomanNumerals/src/test/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverterTest.java
@@ -2,16 +2,34 @@ package de.kifaru.ndegendogo.kata.romanNumbers;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class RomanNumbersConverterTest {
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {"I", 1}
+        });
+    }
+
+    @Parameter(value=0)
+    public String actualInput;
+
+    @Parameter(value=1)
+    public int expectedResult;
 
     @Test
     public void testConvertToArabicNumber() {
-        final String actualInput = "I";
-        final int expectedResult = 1;
         final Integer actualResult = RomanNumbersConverter.convertToArabicNumber(actualInput);
         assertEquals(expectedResult, actualResult.intValue());
     }
-
 }
