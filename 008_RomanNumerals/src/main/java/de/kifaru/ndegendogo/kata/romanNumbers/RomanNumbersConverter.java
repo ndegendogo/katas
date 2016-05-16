a089c34a103e789a47be6e19d84930486bc968d5
ndegendogo <christa.runge@kifaru.de>
2016-05-16 18:59:15 +0200

    Refactor production code: move the bounds check.

--- 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverter.java
+++ 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverter.java
@@ -15,16 +15,19 @@ public class RomanNumbersConverter implements Function<String, Integer> {
     Integer convertToArabicNumber(final String romanNumber) {
         result = 0;
         offset = 0;
-        while (romanNumber.length() > offset && matchDigit(romanNumber, 'X', 10)) {
+        while (matchDigit(romanNumber, 'X', 10)) {
         }
-        while (romanNumber.length() > offset &&  matchDigit(romanNumber, 'V', 5)) {
+        while (matchDigit(romanNumber, 'V', 5)) {
         }
-        while (romanNumber.length() > offset && matchDigit(romanNumber, 'I', 1)) {
+        while (matchDigit(romanNumber, 'I', 1)) {
         }
         return result;
     }
 
     private boolean matchDigit(final String romanNumber, char digit, int value) {
+        if (romanNumber.length() <= offset) {
+            return false;
+        }
         if (romanNumber.charAt(offset) == digit) {
             offset += 1;
             result += value;
