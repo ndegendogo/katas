475fb2e2946270bb753cc06b97e6e9daba8f40b6
ndegendogo <christa.runge@kifaru.de>
2016-05-19 21:46:18 +0200

    Cleanup: make parameter final.

--- 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverter.java
+++ 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanNumbersConverter.java
@@ -2,6 +2,6 @@ package de.kifaru.ndegendogo.kata.romanNumbers;
 
 public interface RomanNumbersConverter {
 
-    public Integer convertToArabicNumber(String romanNumber);
+    public Integer convertToArabicNumber(final String romanNumber);
 
 }
