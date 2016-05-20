dd1f9fcf254c9c20d5d4b48c77ff75e20398ed3b
ndegendogo <christa.runge@kifaru.de>
2016-05-20 20:07:29 +0200

    Add a unit test (red).

--- 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/BowlingScoreTest.java
+++ 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/BowlingScoreTest.java
@@ -1,5 +1,13 @@
 package de.kifaru.ndegendogo.kata.bowling;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class BowlingScoreTest {
 
+    @Test
+    public void testCalculateScore() {
+        fail();
+    }
 }
