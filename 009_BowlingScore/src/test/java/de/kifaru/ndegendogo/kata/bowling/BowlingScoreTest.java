dbf3d95f655549da7a40591ca221f06e90da368c
ndegendogo <christa.runge@kifaru.de>
2016-05-21 22:59:39 +0200

    Refactor test: prepare a parametrized test.

--- 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/BowlingScoreTest.java
+++ 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/BowlingScoreTest.java
@@ -2,19 +2,38 @@ package de.kifaru.ndegendogo.kata.bowling;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class BowlingScoreTest {
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new Integer[][]{{0}}, new Integer[]{0}}
+        });
+    }
 
-    Integer[] calculateScores(Integer[][] rolls) {
-        return new Integer[]{0};
+    private Integer[][] rolls;
+    private Integer[] expectedScores;
+
+    public BowlingScoreTest(final Integer[][] rolls, final Integer[] expectedScores) {
+        this.rolls = rolls;
+        this.expectedScores = expectedScores;
     }
 
     @Test
     public void testCalculateScores() {
-        Integer[][] rolls = {{0}};
-        Integer[] expectedScores = {0};
-        Integer[] actualScores = calculateScores(rolls);
+        final Integer[] actualScores = calculateScores(rolls);
         assertArrayEquals(expectedScores, actualScores);
+    }
+
+    Integer[] calculateScores(final Integer[][] rolls) {
+        return new Integer[]{0};
     }
 }
