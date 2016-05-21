56adb5918308fc2ccb1203f86496ec8f6bf4105c
ndegendogo <christa.runge@kifaru.de>
2016-05-21 22:16:51 +0200

    Implement a very first unit test (green).

--- 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/BowlingScoreTest.java
+++ 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/BowlingScoreTest.java
@@ -6,8 +6,15 @@ import org.junit.Test;
 
 public class BowlingScoreTest {
 
+    Integer[] calculateScores(Integer[][] rolls) {
+        return new Integer[]{0};
+    }
+
     @Test
-    public void testCalculateScore() {
-        fail();
+    public void testCalculateScores() {
+        Integer[][] rolls = {{0}};
+        Integer[] expectedScores = {0};
+        Integer[] actualScores = calculateScores(rolls);
+        assertArrayEquals(expectedScores, actualScores);
     }
 }
