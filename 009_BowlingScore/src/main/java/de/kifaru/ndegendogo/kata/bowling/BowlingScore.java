90b51660a3697d8d1df4d2259039cbed4bec4a68
ndegendogo <christa.runge@kifaru.de>
2016-05-22 18:53:07 +0200

    Refactor: extract a method into production code.

--- 009_BowlingScore/src/main/java/de/kifaru/ndegendogo/kata/bowling/BowlingScore.java
+++ 009_BowlingScore/src/main/java/de/kifaru/ndegendogo/kata/bowling/BowlingScore.java
@@ -5,4 +5,8 @@ public class BowlingScore {
     public static void main(String... args) {
         System.out.println("Hello World!");
     }
+
+    static int calculateScore(final Integer[] rolls) {
+        return 0;
+    }
 }
--- 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/BowlingScoreTest.java
+++ 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/BowlingScoreTest.java
@@ -31,7 +31,7 @@ public class BowlingScoreTest {
 
     @Test
     public void testCalculateScore() {
-        final Integer actualScore = 0;
+        final Integer actualScore = BowlingScore.calculateScore(rolls);
         assertEquals(expectedScore, actualScore);
     }
 
