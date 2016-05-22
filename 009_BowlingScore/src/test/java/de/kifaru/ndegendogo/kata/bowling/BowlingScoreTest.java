73e77e8b43b975187b0d428c4352f24c8539d299
ndegendogo <christa.runge@kifaru.de>
2016-05-22 09:48:10 +0200

    Refactor unit test: inline method.

--- 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/BowlingScoreTest.java
+++ 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/BowlingScoreTest.java
@@ -14,7 +14,17 @@ import org.junit.runners.Parameterized.Parameters;
 public class BowlingScoreTest {
     @Parameters(name="{index}: {2}")
     public static Collection<Object[]> data() {
-        return constructTestCases();
+        final Integer[][] input1 = new Integer[][]{{0}};
+        final Integer[] output1 = new Integer[]{0};
+        Object[] testcase1 = constructSingleTestcase(input1, output1);
+        final Integer[][] input2 = new Integer[][]{{1}};
+        final Integer[] output2 = new Integer[]{1};
+        Object[] testcase2 = constructSingleTestcase(input2, output2);
+        return Arrays.asList(new Object[][]{
+                testcase1,
+                testcase2,
+//                {new Integer[][]{{1, 2}}, new Integer[]{3}},
+        });
     }
 
     private Integer[][] rolls;
@@ -31,20 +41,6 @@ public class BowlingScoreTest {
     public void testCalculateScores() {
         final Integer[] actualScores = BowlingScore.calculateScores(rolls);
         assertArrayEquals(expectedScores, actualScores);
-    }
-
-    private static Collection<Object[]> constructTestCases() {
-        final Integer[][] input1 = new Integer[][]{{0}};
-        final Integer[] output1 = new Integer[]{0};
-        Object[] testcase1 = constructSingleTestcase(input1, output1);
-        final Integer[][] input2 = new Integer[][]{{1}};
-        final Integer[] output2 = new Integer[]{1};
-        Object[] testcase2 = constructSingleTestcase(input2, output2);
-        return Arrays.asList(new Object[][]{
-                testcase1,
-                testcase2,
-//                {new Integer[][]{{1, 2}}, new Integer[]{3}},
-        });
     }
 
     private static Object[] constructSingleTestcase(final Integer[][] rolls, final Integer[] expectedScores) {
