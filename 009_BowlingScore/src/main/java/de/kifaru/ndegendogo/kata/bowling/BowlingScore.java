2e1ecfb287e67cddb466f061b4260895be1df07f
ndegendogo <christa.runge@kifaru.de>
2016-05-22 19:11:15 +0200

    Extend production code: implement the red test case.

--- 009_BowlingScore/src/main/java/de/kifaru/ndegendogo/kata/bowling/BowlingScore.java
+++ 009_BowlingScore/src/main/java/de/kifaru/ndegendogo/kata/bowling/BowlingScore.java
@@ -10,6 +10,10 @@ public class BowlingScore {
         int score = 0;
         for (int index = 0; index < rolls.length; index ++) {
             score += rolls[index];
+            if (rolls[index] == 10) {
+                score += rolls[index +1];
+                score += rolls[index +2];
+            }
         }
         return score;
     }
