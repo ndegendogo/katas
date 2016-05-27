714591bf835126727212ca69d425403b15e585f5
ndegendogo <christa.runge@kifaru.de>
2016-05-28 00:42:44 +0200

    Cleanup unit tests: remove duplicate tests.

--- 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/FrameTest.java
+++ 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/FrameTest.java
@@ -43,58 +43,6 @@ public class FrameTest {
         assertEquals(expectedFrames, frames.toString());
     }
 
-    @Test
-    public void testFramesNoRoll() {
-        Integer[] rolls = {};
-        String expectedFrames = "[]";
-        String name = "testFramesNoRoll";
-        Frame.initFrames();
-        for (Integer roll : rolls) {
-            Frame.rollTheBall(roll);
-        }
-        List<Frame> frames = Frame.getFrames();
-        assertEquals(expectedFrames, frames.toString());
-    }
-
-    @Test
-    public void testFramesFirstRoll() {
-        Integer[] rolls = {1};
-        String expectedFrames = "[[1]]";
-        String name = "testFramesFirstRoll";
-        Frame.initFrames();
-        for (Integer roll : rolls) {
-            Frame.rollTheBall(roll);
-        }
-        List<Frame> frames = Frame.getFrames();
-        assertEquals(expectedFrames, frames.toString());
-    }
-
-    @Test
-    public void testFramesTwoRolls() {
-        Integer[] rolls = {1, 2};
-        String expectedFrames = "[[1|2]]";
-        String name = "testFramesTwoRolls";
-        Frame.initFrames();
-        for (Integer roll : rolls) {
-            Frame.rollTheBall(roll);
-        }
-        List<Frame> frames = Frame.getFrames();
-        assertEquals(expectedFrames, frames.toString());
-    }
-
-    @Test
-    public void testFramesThreeRolls() {
-        Integer[] rolls = {1, 2, 3};
-        String expectedFrames = "[[1|2], [3]]";
-        String name = "testFramesThreeRolls";
-        Frame.initFrames();
-        for (Integer roll : rolls) {
-            Frame.rollTheBall(roll);
-        }
-        List<Frame> frames = Frame.getFrames();
-        assertEquals(expectedFrames, frames.toString());
-    }
-
     static Object[] constructTestcase(final Integer[] rolls, final String expectedFrames, final String name) {
             return new Object[]{rolls, expectedFrames, name};
     }
