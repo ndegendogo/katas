67ea2e69213eb1049fdef80bc02561659fb12cad
ndegendogo <christa.runge@kifaru.de>
2016-05-27 19:08:18 +0200

    Add a first true test on Frame - red.

--- 009_BowlingScore/src/main/java/de/kifaru/ndegendogo/kata/bowling/Frame.java
+++ 009_BowlingScore/src/main/java/de/kifaru/ndegendogo/kata/bowling/Frame.java
@@ -1,5 +1,10 @@
 package de.kifaru.ndegendogo.kata.bowling;
 
+import java.util.List;
+
 
 public class Frame {
+    static List<Frame> initFrames() {
+        return null;
+    }
 }
--- 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/FrameTest.java
+++ 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/FrameTest.java
@@ -2,12 +2,16 @@ package de.kifaru.ndegendogo.kata.bowling;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
+
 
 public class FrameTest {
 
     @Test
     public void testFrames() {
-        fail();
+        List<Frame> frames = Frame.initFrames();
+        assertEquals(0, frames.size());
     }
 }
