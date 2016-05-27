2877b5db91d2129faf1121a77e1a98b67cf7ec5b
ndegendogo <christa.runge@kifaru.de>
2016-05-27 23:46:55 +0200

    Another unit test: three rolls (red).

--- 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/FrameTest.java
+++ 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/FrameTest.java
@@ -34,4 +34,15 @@ public class FrameTest {
         assertEquals(1, frames.size());
         assertEquals("[1|2]", frames.get(0).toString());
     }
+
+    @Test
+    public void testFramesThreeRolls() {
+        Frame.initFrames();
+        Frame.rollTheBall(1);
+        Frame.rollTheBall(2);
+        Frame.rollTheBall(3);
+        List<Frame> frames = Frame.getFrames();
+        assertEquals(2, frames.size());
+//        assertEquals("[1|2]", frames.get(0).toString());
+    }
 }
