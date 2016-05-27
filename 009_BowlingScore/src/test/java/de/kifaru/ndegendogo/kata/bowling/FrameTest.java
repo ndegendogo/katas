b2636c914ded1892c64f4998fb33ff7e5b7ceea7
ndegendogo <christa.runge@kifaru.de>
2016-05-27 19:02:40 +0200

    Add a new class to unit test the Frame (tests are red).

---
+++ 009_BowlingScore/src/test/java/de/kifaru/ndegendogo/kata/bowling/FrameTest.java
@@ -0,0 +1,13 @@
+package de.kifaru.ndegendogo.kata.bowling;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FrameTest {
+
+    @Test
+    public void testFrames() {
+        fail();
+    }
+}
