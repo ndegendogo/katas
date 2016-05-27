740f20027e46ea5172fe15e49079cb10e4f65dee
ndegendogo <christa.runge@kifaru.de>
2016-05-27 22:12:18 +0200

    Extend production code: implement the red test case (green).

--- 009_BowlingScore/src/main/java/de/kifaru/ndegendogo/kata/bowling/Frame.java
+++ 009_BowlingScore/src/main/java/de/kifaru/ndegendogo/kata/bowling/Frame.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class Frame {
     private static List<Frame> frames;
+    private Integer roll1;
 
     static void initFrames() {
         frames = new ArrayList<Frame>();
@@ -16,6 +17,14 @@ public class Frame {
     }
 
     static void rollTheBall(Integer pins) {
-        frames.add(new Frame());
+        frames.add(new Frame(pins));
+    }
+
+    private Frame(Integer pins) {
+        roll1 = pins;
+    }
+
+    public String toString() {
+        return "[" + roll1.toString() + "]";
     }
 }
