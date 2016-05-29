4697e9476f6ca3c301c0298d617cf84035fec9d9
ndegendogo <christa.runge@kifaru.de>
2016-05-30 00:57:32 +0200

    Cleanup: make parameter final.

--- 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanConverterMain.java
+++ 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanConverterMain.java
@@ -3,10 +3,8 @@ package de.kifaru.ndegendogo.kata.romanNumbers;
 import javax.swing.SwingUtilities;
 
 public class RomanConverterMain {
-
-    public static void main(String ... args) {
+    public static void main(final String ... args) {
         final RomanConverterGui gui = new RomanConverterGui(new RomanNumbersConverter());
         SwingUtilities.invokeLater(gui::launch);
     }
-
 }
