843f88210491619a838ef256623f772f422890ae
ndegendogo <christa.runge@kifaru.de>
2016-05-30 23:05:24 +0200

    Move construction of GUI into the invokeLater block.
    It is swing-related and should be executed on the swing event thread.

--- 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanConverterMain.java
+++ 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanConverterMain.java
@@ -4,7 +4,10 @@ import javax.swing.SwingUtilities;
 
 public class RomanConverterMain {
     public static void main(final String ... args) {
-        final RomanConverterGui gui = new RomanConverterGui(new RomanNumbersConverter());
-        SwingUtilities.invokeLater(() -> gui.launch());
+        SwingUtilities.invokeLater(() -> 
+        {
+            final RomanConverterGui gui = new RomanConverterGui(new RomanNumbersConverter());
+            gui.launch();
+        });
     }
 }
