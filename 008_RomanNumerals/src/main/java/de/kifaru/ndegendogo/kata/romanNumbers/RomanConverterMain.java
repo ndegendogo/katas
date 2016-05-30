a1055c688134706a5960ddcece038a5f09096d35
ndegendogo <christa.runge@kifaru.de>
2016-05-30 22:59:37 +0200

    Refactor: convert method reference back to a lambda expression.
    We need to group statements differently.

--- 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanConverterMain.java
+++ 008_RomanNumerals/src/main/java/de/kifaru/ndegendogo/kata/romanNumbers/RomanConverterMain.java
@@ -5,6 +5,6 @@ import javax.swing.SwingUtilities;
 public class RomanConverterMain {
     public static void main(final String ... args) {
         final RomanConverterGui gui = new RomanConverterGui(new RomanNumbersConverter());
-        SwingUtilities.invokeLater(gui::launch);
+        SwingUtilities.invokeLater(() -> gui.launch());
     }
 }
